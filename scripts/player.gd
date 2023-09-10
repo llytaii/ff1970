0ae68a278db7a81225bf204626801522a27623e2
llytaii <llytaii@pm.me>
2023-09-10 11:36:04 +0200

    initial commit

---
+++ scripts/player.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+@onready var anim = $AnimatedSprite2D
+
+const speed = 250.0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_pressed("go_up"):
+		self.translate(Vector2.UP * speed * delta)
+		anim.play("go_up")
+	elif Input.is_action_pressed("go_down"):
+		self.translate(Vector2.DOWN * speed * delta)
+		anim.play("go_down")
+	elif Input.is_action_pressed("go_left"):
+		self.translate(Vector2.LEFT * speed * delta)
+		anim.play("go_left")
+	elif Input.is_action_pressed("go_right"):
+		self.translate(Vector2.RIGHT * speed * delta)
+		anim.play("go_right")
+	pass
